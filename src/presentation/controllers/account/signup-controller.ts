d54efaa6c905addc5e2f0d8a44d730d668f26e5c
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-07 13:27:05 -0300

    feat: finish signup controller

--- src/presentation/controllers/account/signup-controller.ts
+++ src/presentation/controllers/account/signup-controller.ts
@@ -1,12 +1,17 @@
 import { AddAccount } from '@/domain/usecases/account/add-account'
 import { EmailInUseError } from '@/presentation/errors/email-in-use-error'
-import { forbidden, ok, serverError } from '@/presentation/helpers'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class SignUpController implements Controller {
   constructor (private readonly addAccount: AddAccount) {}
   async handle (request: SignUpController.Request): Promise<HttpResponse> {
     try {
+      if (!request.name) return badRequest(new MissingParamError('name'))
+      if (!request.email) return badRequest(new MissingParamError('email'))
+      if (!request.password) return badRequest(new MissingParamError('password'))
+
       const res = await this.addAccount.add(request)
       if (!res) return forbidden(new EmailInUseError())
       return ok(res)
