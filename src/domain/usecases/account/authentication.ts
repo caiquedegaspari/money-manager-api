dd076999abfce7dab240cadae9afd3f11a58b677
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-07 13:30:05 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/account/authentication.ts
@@ -0,0 +1,14 @@
+export interface Authentication {
+  auth: (authentication: Authentication.Params) => Promise<Authentication.Result | null>
+}
+
+export namespace Authentication {
+  export type Params = {
+    email: string
+    password: string
+  }
+  export type Result = {
+    accessToken: string
+    name: string
+  }
+}
