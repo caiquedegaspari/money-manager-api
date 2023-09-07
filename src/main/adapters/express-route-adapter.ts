6908b91014ad56641aee00b4a8c5e38e0315e614
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-07 13:26:38 -0300

    feat: finish rout config

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -7,6 +7,7 @@ export const adaptRoute = (controller: Controller) => {
       ...(req.body || {}),
       ...(req.params || {})
     }
+
     const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
