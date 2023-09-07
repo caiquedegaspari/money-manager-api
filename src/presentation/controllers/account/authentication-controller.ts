70588c30c0834edd9532457bc229ddfddeecf72c
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-07 15:04:10 -0300

    feat: ensure AuthenticationController returns 500 if Authentication throws

--- src/presentation/controllers/account/authentication-controller.ts
+++ src/presentation/controllers/account/authentication-controller.ts
@@ -1,14 +1,17 @@
 import { Authentication } from '@/domain/usecases/account/authentication'
-import { ok, unauthorized } from '@/presentation/helpers'
+import { ok, serverError, unauthorized } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class AuthenticationController implements Controller {
   constructor (private readonly authentication: Authentication) {}
-
   async handle (request: AuthenticationController.Params): Promise<HttpResponse> {
-    const response = await this.authentication.auth(request)
-    if (!response) return unauthorized()
-    return ok(response)
+    try {
+      const response = await this.authentication.auth(request)
+      if (!response) return unauthorized()
+      return ok(response)
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
 
