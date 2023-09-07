f67ee4af63f9e7f1659045e2ef9219060e4d1fda
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-07 12:59:41 -0300

    feat: finish routes/module alias config

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import 'module-alias/register'
 
-app.listen(4000, () => console.log('💸 Money Manager running at http://localhost:4000 💸'))
+const boostrap = async (): Promise<void> => {
+  const app = (await import('./config/app')).default
+  app.listen(4000, () => console.log('💸 Money Manager running at http://localhost:4000 💸'))
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+boostrap()
