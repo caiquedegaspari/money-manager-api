7e4daab510966e33d191aad283496434fe649d93
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-07 14:05:28 -0300

    feat: ensure DbAuthentication returns null if HashComparer returns false

--- src/data/usecases/account/db-authentication.ts
+++ src/data/usecases/account/db-authentication.ts
@@ -1,11 +1,18 @@
+import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/account'
 import { Authentication } from '@/domain/usecases/account/authentication'
 
 export class DbAuthentication implements Authentication {
-  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
+
   async auth (authentication: Authentication.Params): Promise<Authentication.Result | null> {
     const user = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (!user) return null
+    const isValidPassword = await this.hashComparer.compare(authentication.password, user.password)
+    if (!isValidPassword) return null
     return await Promise.resolve({
       accessToken: 'access',
       name: 'name'
