6f58ee193c36737091135071f46278ca6772f6b0
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-09 11:05:40 -0300

    feat: add updateMonthIncome interface

---
+++ src/domain/usecases/account/update-month-income.ts
@@ -0,0 +1,11 @@
+interface UpdateMonthIncome {
+  updateMonthIncome: (params: UpdateMonthIncome.Params) => Promise<UpdateMonthIncome.Result>
+}
+
+export namespace UpdateMonthIncome {
+  export type Params = {
+    userId: number
+    value: number
+  }
+  export type Result = number
+}
