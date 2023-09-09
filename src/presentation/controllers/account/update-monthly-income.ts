f2a6520c4286a89d43521dea0d77d3121d028520
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-09 12:21:28 -0300

    feat: ensure UpdateMonthlyIncomeController returns 400 if monthly income is less than 100

--- src/presentation/controllers/account/update-monthly-income.ts
+++ src/presentation/controllers/account/update-monthly-income.ts
@@ -1,4 +1,5 @@
 import { UpdateMonthlyIncome } from '@/domain/usecases/account/update-month-income'
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { UserNotFoundError } from '@/presentation/errors/user-not-found-error'
 import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers'
@@ -10,6 +11,7 @@ export class UpdateMonthlyIncomeController implements Controller {
     try {
       if (!request.userId) return badRequest(new MissingParamError('userId'))
       if (!request.value) return badRequest(new MissingParamError('value'))
+      if (request.value <= 100) return badRequest(new InvalidParamError('monthly income must be greater than 100'))
       const monthlyIncome = await this.updateMonthlyIncome.updateMonthlyIncome(request)
       if (!monthlyIncome) return forbidden(new UserNotFoundError())
       return ok(20)
