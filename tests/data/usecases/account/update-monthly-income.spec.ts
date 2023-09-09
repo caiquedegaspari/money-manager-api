db4a0c83ac347c84b85ccf55959ce797e539c36d
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-09 11:36:50 -0300

    test: ensure DbUpdateMonthIncome calls UpdateMonthlyIncomeRepository with correct values

---
+++ tests/data/usecases/account/update-monthly-income.spec.ts
@@ -0,0 +1,65 @@
+import { UpdateMonthlyIncome } from '@/domain/usecases/account/update-month-income'
+import { LoadAccountByIdRepository } from '@/data/protocols/db/account'
+import { DbUpdateMonthlyIncome } from '@/data/usecases/account/db-update-monthly-income'
+import { UpdateMonthlyIncomeRepository } from '@/data/protocols/db/account/update-monthly-income-repository'
+
+interface SutTypes {
+  sut: UpdateMonthlyIncome
+  loadAccountByIdRepositoryStub: LoadAccountByIdRepository
+  updateMonthlyIncomeRepositoryStub: UpdateMonthlyIncomeRepository
+}
+
+const mockLoadAccountByIdRepository = (): LoadAccountByIdRepository => {
+  class LoadAccountByIdRepositoryStub implements LoadAccountByIdRepository {
+    async loadById (id: LoadAccountByIdRepository.Params): Promise<LoadAccountByIdRepository.Result> {
+      return await Promise.resolve({
+        id: 1,
+        email: 'any_mail@mail.com'
+      })
+    }
+  }
+  return new LoadAccountByIdRepositoryStub()
+}
+
+const mockUpdateMonthlyIncomeRepository = (): UpdateMonthlyIncomeRepository => {
+  class UpdateMonthlyIncomeRepositoryStub implements UpdateMonthlyIncomeRepository {
+    async updateMonthlyIncome (id: UpdateMonthlyIncomeRepository.Params): Promise<UpdateMonthlyIncomeRepository.Result> {
+      return await Promise.resolve(2000)
+    }
+  }
+  return new UpdateMonthlyIncomeRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByIdRepositoryStub = mockLoadAccountByIdRepository()
+  const updateMonthlyIncomeRepositoryStub = mockUpdateMonthlyIncomeRepository()
+  const sut = new DbUpdateMonthlyIncome(loadAccountByIdRepositoryStub, updateMonthlyIncomeRepositoryStub)
+
+  return {
+    loadAccountByIdRepositoryStub,
+    updateMonthlyIncomeRepositoryStub,
+    sut
+  }
+}
+
+describe('DbUpdateMonthIncome', () => {
+  it('Should call LoadAccountByIdRepository with correct values', async () => {
+    const { sut, loadAccountByIdRepositoryStub } = makeSut()
+    const spyLoad = jest.spyOn(loadAccountByIdRepositoryStub, 'loadById')
+    await sut.updateMonthlyIncome({ userId: 1, value: 200 })
+    expect(spyLoad).toHaveBeenCalledWith(1)
+  })
+  it('Should return null if LoadAccountByIdRepository returns null', async () => {
+    const { sut, loadAccountByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByIdRepositoryStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const result = await sut.updateMonthlyIncome({ userId: 1, value: 200 })
+    expect(result).toBeNull()
+  })
+
+  it('Should call UpdateMonthIncomeRepository with correct values', async () => {
+    const { sut, updateMonthlyIncomeRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateMonthlyIncomeRepositoryStub, 'updateMonthlyIncome')
+    await sut.updateMonthlyIncome({ userId: 1, value: 1000 })
+    expect(updateSpy).toHaveBeenCalledWith({ userId: 1, value: 1000 })
+  })
+})
