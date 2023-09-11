3ac2331c1b8de1c572af41475286e5ab4750ce61
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-11 19:49:29 -0300

    feat: ensure AddExpenseController returns 500 if AddExpense throws

--- src/presentation/controllers/expense/add-expense-controller.ts
+++ src/presentation/controllers/expense/add-expense-controller.ts
@@ -1,19 +1,23 @@
 import { AddExpense } from '@/domain/usecases/expenses/add-expense'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
-import { badRequest, ok } from '@/presentation/helpers'
+import { badRequest, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class AddExpenseController implements Controller {
   constructor (private readonly addExpense: AddExpense) {}
   async handle (request: AddExpenseController.Params): Promise<HttpResponse> {
-    if (!request.name) return badRequest(new MissingParamError('name'))
-    if (!request.value) return badRequest(new MissingParamError('value'))
-    if (!request.date) return badRequest(new MissingParamError('date'))
-    if (request.value < 1) return badRequest(new InvalidParamError('value must be greater than 0'))
+    try {
+      if (!request.name) return badRequest(new MissingParamError('name'))
+      if (!request.value) return badRequest(new MissingParamError('value'))
+      if (!request.date) return badRequest(new MissingParamError('date'))
+      if (request.value < 1) return badRequest(new InvalidParamError('value must be greater than 0'))
 
-    const expense = await this.addExpense.add(request)
-    return ok(expense)
+      const expense = await this.addExpense.add(request)
+      return ok(expense)
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
 
