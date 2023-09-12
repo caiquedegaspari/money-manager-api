36e3bb8419e04efc1ce3f0c90e768b56608d4209
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-12 19:22:14 -0300

    feat: add listExpenses usecase domain

---
+++ src/domain/usecases/expenses/list-expenses.ts
@@ -0,0 +1,27 @@
+export interface ListExpenses {
+  list: (params: ListExpenses.Params) => Promise<ListExpenses.Result>
+}
+
+type Expense = {
+  name: string
+  value: number
+  category: string
+}
+
+type CategoryPercentage = {
+  category: string
+  percent: number
+  totalSpent: number
+}
+
+export namespace ListExpenses {
+  export type Params = {
+    startDate: Date
+    endDate: Date
+    userId: number
+  }
+  export type Result = {
+    expenses: Expense[]
+    percentages: CategoryPercentage[]
+  }
+}
