f33af1df17710b3d8750695c6ed370f1c0fc6ba8
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-12 20:05:04 -0300

    feat: ensure DbListExpenses calls returns correct expenses on success

--- src/data/usecases/expenses/db-list-expenses.ts
+++ src/data/usecases/expenses/db-list-expenses.ts
@@ -4,17 +4,7 @@ import { ListExpenses } from '@/domain/usecases/expenses/list-expenses'
 export class DbListExpenses implements ListExpenses {
   constructor (private readonly listExpensesRepository: ListExpensesRepository) {}
   async list (params: ListExpenses.Params): Promise<ListExpenses.Result> {
-    await this.listExpensesRepository.list(params)
-    return await Promise.resolve({
-      expenses: [{
-        name: 'any',
-        value: 200
-      }],
-      percentages: [{
-        category: 'any category',
-        percent: 100,
-        totalSpent: 200
-      }]
-    })
+    const expenses = await this.listExpensesRepository.list(params)
+    return { expenses, percentages: [] }
   };
 }
