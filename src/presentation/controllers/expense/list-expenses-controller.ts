7ba68c2296f32c24dd7e2dd7ea4a744908ee7ebf
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-13 19:55:17 -0300

    feat: ensure ListExpensesController calls ListExpense with correct values

---
+++ src/presentation/controllers/expense/list-expenses-controller.ts
@@ -0,0 +1,19 @@
+import { ListExpenses } from '@/domain/usecases/expenses/list-expenses'
+import { ok } from '@/presentation/helpers'
+import { Controller, HttpResponse } from '@/presentation/protocols'
+
+export class ListExpensesController implements Controller {
+  constructor (private readonly listExpenses: ListExpenses) {}
+  async handle (request: ListExpensesController.Params): Promise<HttpResponse> {
+    await this.listExpenses.list(request)
+    return ok(200)
+  }
+}
+
+export namespace ListExpensesController {
+  export type Params = {
+    userId: number
+    startDate: Date
+    endDate: Date
+  }
+}
