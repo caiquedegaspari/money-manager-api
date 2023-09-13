5662bd0d3312262261b9331b82888d125b446f9e
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-13 20:08:06 -0300

    feat: ensure ListExpensesCOntroller returns 400 when missing params

--- src/presentation/controllers/expense/list-expenses-controller.ts
+++ src/presentation/controllers/expense/list-expenses-controller.ts
@@ -1,11 +1,16 @@
 import { ListExpenses } from '@/domain/usecases/expenses/list-expenses'
-import { ok, serverError } from '@/presentation/helpers'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { badRequest, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class ListExpensesController implements Controller {
   constructor (private readonly listExpenses: ListExpenses) {}
   async handle (request: ListExpensesController.Params): Promise<HttpResponse> {
     try {
+      if (!request.endDate) badRequest(new MissingParamError('endDate'))
+      if (!request.startDate) badRequest(new MissingParamError('startDate'))
+      if (!request.userId) badRequest(new MissingParamError('userId'))
+
       const expenses = await this.listExpenses.list(request)
       return ok(expenses)
     } catch (err) {
