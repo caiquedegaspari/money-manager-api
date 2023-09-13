6e0a17fee893a3bff80db4c791a57a583c548f16
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-13 19:38:31 -0300

    feat: implement ListExpensesRepository in infra layer

--- src/infra/db/prisma/expense/expense-prisma-repository.ts
+++ src/infra/db/prisma/expense/expense-prisma-repository.ts
@@ -1,8 +1,8 @@
-import { AddExpenseRepository } from '@/data/protocols/db/expenses'
+import { AddExpenseRepository, ListExpensesRepository } from '@/data/protocols/db/expenses'
 import prisma from '../client'
 import { AddManyExpensesRepository } from '@/data/protocols/db/expenses/add-many-expenses-repository'
 
-export class ExpensePrismaRepository implements AddExpenseRepository, AddManyExpensesRepository {
+export class ExpensePrismaRepository implements AddExpenseRepository, AddManyExpensesRepository, ListExpensesRepository {
   async add (params: AddExpenseRepository.Params): Promise<AddExpenseRepository.Result> {
     const createdExpense = await prisma.expense.create({ data: params })
     return {
@@ -22,4 +22,21 @@ export class ExpensePrismaRepository implements AddExpenseRepository, AddManyExp
       categoryId: params[0].categoryId ?? undefined
     }
   };
+
+  async list (params: ListExpensesRepository.Params): Promise<ListExpensesRepository.Result> {
+    const expenses = await prisma.expense.findMany({
+      where: {
+        userId: params.userId,
+        date: { gte: params.startDate, lte: params.endDate }
+      },
+      include: {
+        category: true
+      }
+    })
+    return expenses.map((expense) => ({
+      name: expense.name,
+      value: expense.value,
+      category: expense.category?.name
+    }))
+  }
 }
