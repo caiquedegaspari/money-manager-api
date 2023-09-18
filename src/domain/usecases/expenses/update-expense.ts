ca8bf29e83356407b54ede205582ccdac764cd3b
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-18 19:13:30 -0300

    feat: add updateExpense usecase domain layer

---
+++ src/domain/usecases/expenses/update-expense.ts
@@ -0,0 +1,11 @@
+export interface UpdateExpense {
+  update: (params: UpdateExpense.Params) => UpdateExpense.Result
+}
+
+export namespace UpdateExpense {
+  export type Params = {
+    expenseId: number
+    value: number
+  }
+  export type Result = Promise<void>
+}
