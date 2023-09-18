886ba390a61dd17be44ee2c6700054373321e002
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-18 19:26:45 -0300

    test: ensure UpdateExpense calls LoadExpenseByIdRepository with correct values

---
+++ tests/data/usecases/expenses/update-expense.spec.ts
@@ -0,0 +1,40 @@
+import { LoadExpenseByIdRepository } from '@/data/protocols/db/expenses/load-expense-by-id-repository'
+import { UpdateExpense } from '@/domain/usecases/expenses/update-expense'
+import { DbUpdateExpense } from '@/data/usecases/expenses/db-update-expense'
+
+export type SutTypes = {
+  sut: UpdateExpense
+  loadExpenseByIdRepositoryStub: LoadExpenseByIdRepository
+}
+
+const makeLoadExpenseByIdRepository = (): LoadExpenseByIdRepository => {
+  class LoadExpenseByIdRepositoryStub implements LoadExpenseByIdRepository {
+    async loadById (params: LoadExpenseByIdRepository.Params): Promise<LoadExpenseByIdRepository.Result> {
+      return await Promise.resolve({
+        id: 1,
+        name: 'expense',
+        value: 1000
+      })
+    }
+  }
+  return new LoadExpenseByIdRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadExpenseByIdRepositoryStub = makeLoadExpenseByIdRepository()
+  const sut = new DbUpdateExpense(loadExpenseByIdRepositoryStub)
+
+  return {
+    sut,
+    loadExpenseByIdRepositoryStub
+  }
+}
+
+describe('Update Expense usecase', () => {
+  it('Should LoadExpeseByIdRepository with correct values', async () => {
+    const { loadExpenseByIdRepositoryStub, sut } = makeSut()
+    const loadSpy = jest.spyOn(loadExpenseByIdRepositoryStub, 'loadById')
+    await sut.update({ expenseId: 1, value: 200 })
+    expect(loadSpy).toHaveBeenCalledWith(1)
+  })
+})
