260bfc90616c15bd5a8fccefd79d31c0259228d8
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-25 19:27:29 -0300

    feat: ensure UpdateExpenseController throws if UpdateExpense throws

--- src/presentation/controllers/expense/update-expense-controller.ts
+++ src/presentation/controllers/expense/update-expense-controller.ts
@@ -1,12 +1,16 @@
 import { UpdateExpense } from '@/domain/usecases/expenses/update-expense'
-import { ok } from '@/presentation/helpers'
+import { ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class UpdateExpenseController implements Controller {
   constructor (private readonly updateExpense: UpdateExpense) {}
   async handle (request: UpdateExpenseController.Params): Promise<HttpResponse> {
-    await this.updateExpense.update(request)
-    return ok(true)
+    try {
+      await this.updateExpense.update(request)
+      return ok(true)
+    } catch (err) {
+      return serverError(err as Error)
+    }
   };
 }
 
