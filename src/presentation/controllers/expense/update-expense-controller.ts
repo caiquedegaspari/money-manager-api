7a67b8dabc01a4ac6cf2388c5522c702d958cc67
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-25 19:30:57 -0300

    feat: ensure UpdateExpenseController returns 400 on invalid parameters

--- src/presentation/controllers/expense/update-expense-controller.ts
+++ src/presentation/controllers/expense/update-expense-controller.ts
@@ -1,11 +1,13 @@
 import { UpdateExpense } from '@/domain/usecases/expenses/update-expense'
-import { ok, serverError } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
+import { badRequest, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class UpdateExpenseController implements Controller {
   constructor (private readonly updateExpense: UpdateExpense) {}
   async handle (request: UpdateExpenseController.Params): Promise<HttpResponse> {
     try {
+      if (request.value < 0) return badRequest(new InvalidParamError('value must be greater than 0'))
       await this.updateExpense.update(request)
       return ok(true)
     } catch (err) {
