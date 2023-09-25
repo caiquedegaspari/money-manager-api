3a6ffc544ab72c9d3c7a90fe2ed5d21023074974
Caique Degaspari <caique.degaspare@gmail.com>
2023-09-25 19:27:51 -0300

    test: ensure UpdateExpenseController throws if UpdateExpense throws

--- tests/presentation/controllers/expense/update-expense-controller.spec.ts
+++ tests/presentation/controllers/expense/update-expense-controller.spec.ts
@@ -1,10 +1,13 @@
 import { UpdateExpense } from '@/domain/usecases/expenses/update-expense'
 import { UpdateExpenseController } from '@/presentation/controllers/expense/update-expense-controller'
+import { serverError } from '@/presentation/helpers'
 
 type SutTypes = {
   updateExpenseStub: UpdateExpense
   sut: UpdateExpenseController
 }
+
+const mockRequest = (): UpdateExpense.Params => ({ expenseId: 1, value: 200 })
 
 const mockUpdateExpense = (): UpdateExpense => {
   class UpdateExpenseStub implements UpdateExpense {
@@ -25,7 +28,15 @@ describe('Update Expense Controller', () => {
   it('Should call UpdateExpense', async () => {
     const { sut, updateExpenseStub } = makeSut()
     const updateSpy = jest.spyOn(updateExpenseStub, 'update')
-    await sut.handle({ expenseId: 1, value: 200 })
-    expect(updateSpy).toHaveBeenCalledWith({ expenseId: 1, value: 200 })
+    const params = mockRequest()
+    await sut.handle(params)
+    expect(updateSpy).toHaveBeenCalledWith(params)
+  })
+  it('Should return 500 if UpdateExpenses throws', async () => {
+    const { updateExpenseStub, sut } = makeSut()
+    jest.spyOn(updateExpenseStub, 'update').mockReturnValueOnce(Promise.reject(new Error()))
+
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(serverError(new Error()))
   })
 })
